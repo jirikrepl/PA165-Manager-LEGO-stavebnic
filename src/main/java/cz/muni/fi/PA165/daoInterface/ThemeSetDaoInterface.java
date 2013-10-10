7d5fa3c362f4b51b3d4bba95c5988091ff66f73f
xbako <bako.pavol@gmail.com>
2013-10-10 23:19:29 +0200

    JAVA DOC added
    ThemeSetDaoInterface ThemeSetDAO

--- src/main/java/cz/muni/fi/PA165/daoInterface/ThemeSetDaoInterface.java
+++ src/main/java/cz/muni/fi/PA165/daoInterface/ThemeSetDaoInterface.java
@@ -10,12 +10,32 @@ import java.util.List;
  */
 public interface ThemeSetDaoInterface {
     
+    /**
+     * creates and updates ThemeSet instance
+     *
+     * @param set instance of ThemeSet entity class, which has to be changed
+     */
     public void createOrUpdateThemeSet(ThemeSet set);
     
+    /**
+     * removes the ThemeSet instance
+     *
+     * @param set instance of ThemeSet entity class
+     */
     public void removeThemeSet(ThemeSet set);
     
+    /**
+     * finds all entities in DB
+     *
+     * @return List<ThemeSet> all entities in DB
+     */
     public List<ThemeSet> findAll();
     
+    /**
+     * finds entities in DB with given price
+     *
+     * @return List<ThemeSet> of all entities with given price
+     */
     public List<ThemeSet> findByPrice(BigDecimal price);
     
 }
