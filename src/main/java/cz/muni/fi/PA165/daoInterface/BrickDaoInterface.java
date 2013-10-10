22f5381accbcab4cc2f757e939c8a9c328b177d5
jirikrepl <jiri.krepl@gmail.com>
2013-10-10 18:56:18 +0200

    oprava

--- src/main/java/cz/muni/fi/PA165/daoInterface/BrickDaoInterface.java
+++ src/main/java/cz/muni/fi/PA165/daoInterface/BrickDaoInterface.java
@@ -3,7 +3,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package cz.muni.fi.PA165.domain.daoInterface;
+package cz.muni.fi.PA165.daoInterface;
 
 import cz.muni.fi.PA165.domain.Brick;
 import cz.muni.fi.PA165.domain.Color;
