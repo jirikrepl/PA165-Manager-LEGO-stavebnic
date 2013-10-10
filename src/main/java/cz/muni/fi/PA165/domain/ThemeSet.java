d17dae982a85dc9ea7c06c86715807bb7fb356cc
xbako <bako.pavol@gmail.com>
2013-10-10 19:34:29 +0200

    pridani ThemeSet

---
+++ src/main/java/cz/muni/fi/PA165/domain/ThemeSet.java
@@ -0,0 +1,75 @@
+package cz.muni.fi.PA165.domain;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author PALO
+ */
+@Entity
+public class ThemeSet implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    
+    private BigDecimal price;
+    
+    private String description;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof ThemeSet)) {
+            return false;
+        }
+        ThemeSet other = (ThemeSet) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "cz.muni.fi.PA165.domain.ThemeSet[ id=" + id + " ]";
+    }
+    
+}
