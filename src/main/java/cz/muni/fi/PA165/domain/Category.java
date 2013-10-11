2fa3be84466ff6b1a7889b34312299df2dbaef77
Martin Rumanek <rumanekm@localhost.localdomain>
2013-10-11 18:56:13 +0200

    test for CategoryDao, implemented findAll in categoryDaoImpl 	# Please enter the commit message for your changes. Lines starting

--- src/main/java/cz/muni/fi/PA165/domain/Category.java
+++ src/main/java/cz/muni/fi/PA165/domain/Category.java
@@ -5,6 +5,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.io.Serializable;
+import javax.persistence.Column;
 
 /**
  * @author: Martin Rumanek
@@ -14,13 +15,14 @@ import java.io.Serializable;
 public class Category implements Serializable {
 
     private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    @Column(unique=true)
     private String name;
     private String description;
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    public Long getId() {
+        public Long getId() {
         return id;
     }
 
