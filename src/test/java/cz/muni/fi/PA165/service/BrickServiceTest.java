852e5f8940231eef0a6d3d500748f9bce83b23c8
Martin Rumánek <martin@rumanek.cz>
2013-10-28 19:45:39 +0100

    autowired

---
+++ src/test/java/cz/muni/fi/PA165/service/BrickServiceTest.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package cz.muni.fi.PA165.service;
+
+import cz.muni.fi.PA165.domain.Brick;
+import junit.framework.TestCase;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ *
+ * @author Jiri
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:applicationContext.xml"})
+public class BrickServiceTest {
+
+    @Autowired
+    private BrickService service;
+
+
+    @Test
+    public void testBlah() {
+        try {
+            service.create(new Brick());
+        } catch (UnsupportedOperationException e) {
+
+        }
+
+        int x = 1;
+    }
+
+    // TODO add test methods here. The name must begin with 'test'. For example:
+    // public void testHello() {}
+}
