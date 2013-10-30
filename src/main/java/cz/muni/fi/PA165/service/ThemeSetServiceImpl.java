3eccce44121cf91f1ba4840cdb7a4d6ca630b1d8
xbako <bako.pavol@gmail.com>
2013-10-30 08:44:44 +0100

    ThemeSet Service interface and impl

---
+++ src/main/java/cz/muni/fi/PA165/service/ThemeSetServiceImpl.java
@@ -0,0 +1,48 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.muni.fi.PA165.service;
+
+import cz.muni.fi.PA165.dao.ThemeSetDao;
+import cz.muni.fi.PA165.domain.ThemeSet;
+import java.math.BigDecimal;
+import java.util.List;
+
+/**
+ *
+ * @author PALO
+ */
+public class ThemeSetServiceImpl implements ThemeSetService{
+    
+    private ThemeSetDao themeSetDao;
+
+    public void setThemeSetDao(ThemeSetDao themeSetDao) {
+        this.themeSetDao = themeSetDao;
+    }
+
+    public List<ThemeSet> findAll() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public List<ThemeSet> findByPrice(BigDecimal price) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void update(ThemeSet set) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void delete(Long id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void create(ThemeSet set) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public ThemeSet findById(Long id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
