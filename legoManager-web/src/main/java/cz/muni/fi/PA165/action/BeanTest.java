3f289a364f9a5ea94b3a528b1d2307425175dcdf
Martin Rumánek <martin@rumanek.cz>
2013-11-19 22:55:16 +0100

    testovaci beana

---
+++ legoManager-web/src/main/java/cz/muni/fi/PA165/action/BeanTest.java
@@ -0,0 +1,11 @@
+package cz.muni.fi.PA165.action;
+
+/**
+ * @author: Martin Rumanek
+ * @version: 11/19/13
+ */
+public class BeanTest {
+    public String getGreeting() {
+        return "Haf haf";
+    }
+}
