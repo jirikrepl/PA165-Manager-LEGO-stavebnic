3e067187e636262261a77047b931824fdc27225c
Jiri Krepl <jiri.krepl@seznam.cz>
2013-11-22 18:17:32 +0100

    minor changes

--- legoManager-api/src/main/java/cz/muni/fi/PA165/api/service/Color.java
+++ legoManager-api/src/main/java/cz/muni/fi/PA165/api/service/Color.java
@@ -1,7 +1,5 @@
 package cz.muni.fi.PA165.api.service;
 
-
-
 /**
  * enum representing colors of brick
  *
@@ -9,7 +7,32 @@ package cz.muni.fi.PA165.api.service;
  */
 public enum Color {
 
-    WHITE, BRICKYELLOW, NOUGAT, BRIGHTRED, BRIGHTBLUE, BRIGHTYELLOW, BLACK,
-    DARKGREEN, BRIGHTGREEN, DARKORANGE, MEDIUMBLUE, COOLYELLOW, DARKBROWN,
-    OLIVEGREEN, LIGHTPURPLE, BRIGHTPURPLE, MEDIUMLILAC
+    WHITE("brick.color.WHITE"),
+    BRICKYELLOW("brick.color.BRICKYELLOW"),
+    NOUGAT("brick.color.NOUGAT"),
+    BRIGHTRED("brick.color.BRIGHTRED"),
+    BRIGHTBLUE("brick.color.BRIGHTBLUE"),
+    BRIGHTYELLOW("brick.color.BRIGHTYELLOW"),
+    BLACK("brick.color.BLACK"),
+    DARKGREEN("brick.color.DARKGREEN"),   
+    BRIGHTGREEN("brick.color.BRIGHTGREEN"),
+    DARKORANGE("brick.color.DARKORANGE"),
+    
+    MEDIUMBLUE("brick.color.MEDIUMBLUE"), 
+    COOLYELLOW("brick.color.COOLYELLOW"),
+    DARKBROWN("brick.color.DARKBROWN"),
+    OLIVEGREEN("brick.color.OLIVEGREEN"),
+    LIGHTPURPLE("brick.color.LIGHTPURPLE"),
+    BRIGHTPURPLE("brick.color.BRIGHTPURPLE"),
+    MEDIUMLILAC("brick.color.MEDIUMLILAC");
+    
+    private final String text;
+
+    Color(String text) {
+        this.text = text;
+    }
+    
+    public String toString(Object... o) {
+        return String.format(text, o);
+    }
 }
