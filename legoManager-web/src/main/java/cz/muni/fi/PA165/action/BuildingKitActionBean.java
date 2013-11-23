e0cf9dc12bb3a9bf08f9467a042806001c4824e4
tkopecky <kopecky.tomas.ub@seznam.cz>
2013-11-23 04:43:17 +0100

    buildingKitActionBean validace

--- legoManager-web/src/main/java/cz/muni/fi/PA165/action/BuildingKitActionBean.java
+++ legoManager-web/src/main/java/cz/muni/fi/PA165/action/BuildingKitActionBean.java
@@ -69,8 +69,8 @@ public class BuildingKitActionBean extends BaseActionBean {
     @ValidateNestedProperties(
             value = {
                     @Validate(on = {"createBuildingKit", "updateBuildingKit"}, field = "name", required = true, maxlength = 50),
-                    @Validate(on = {"createBuildingKit", "updateBuildingKit"}, field = "yearFrom", minvalue = 0, maxvalue = 100),
-                    @Validate(on = {"createBuildingKit", "updateBuildingKit"}, field = "price", minvalue = 0)
+                    @Validate(on = {"createBuildingKit", "updateBuildingKit"}, field = "yearFrom", required = true, minvalue = 0, maxvalue = 100),
+                    @Validate(on = {"createBuildingKit", "updateBuildingKit"}, field = "price", required = true, minvalue = 0)
             }
     )
     private BuildingKitDto buildingKit;
