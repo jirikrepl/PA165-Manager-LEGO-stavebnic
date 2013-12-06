5f06e8c2ac64646702ea2a15a0a4130033190b96
Martin Rumanek <rumanekm@localhost.localdomain>
2013-12-06 20:42:12 +0100

    completed brick command line interface

---
+++ legoManager-rest-client/src/main/java/cz/muni/fi/PA165/Color.java
@@ -0,0 +1,37 @@
+package cz.muni.fi.PA165;
+
+/**
+ * enum representing colors of brick
+ *
+ * @author jirikrepl
+ */
+public enum Color {
+
+    WHITE("Color.WHITE %s"),
+    BRICKYELLOW("Color.BRICKYELLOW"),
+    NOUGAT("Color.NOUGAT"),
+    BRIGHTRED("Color.BRIGHTRED"),
+    BRIGHTBLUE("Color.BRIGHTBLUE"),
+    BRIGHTYELLOW("Color.BRIGHTYELLOW"),
+    BLACK("Color.BLACK"),
+    DARKGREEN("Color.DARKGREEN"),   
+    BRIGHTGREEN("Color.BRIGHTGREEN"),
+    DARKORANGE("Color.DARKORANGE"),    
+    MEDIUMBLUE("Color.MEDIUMBLUE"), 
+    COOLYELLOW("Color.COOLYELLOW"),
+    DARKBROWN("Color.DARKBROWN"),
+    OLIVEGREEN("Color.OLIVEGREEN"),
+    LIGHTPURPLE("Color.LIGHTPURPLE"),
+    BRIGHTPURPLE("Color.BRIGHTPURPLE"),
+    MEDIUMLILAC("Color.MEDIUMLILAC");
+    
+    private final String text;
+
+    Color(String text) {
+        this.text = text;
+    }
+    
+    public String toString(Object... o) {
+        return String.format(text, o);
+    }
+}
